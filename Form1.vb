0aa185dd8c61cdb730af2cfbd6c92fadb15da166
LockMan007 <LockMan007@users.noreply.github.com>
2023-08-10 21:14:16 -0400

    Add files via upload
    Facebook Messenger that is simply a web browser that opens "facebook.com/messages". This allows Drag and Drop of images, for people who the Windows Version just shows up as an icon instead.
    
    This uses WebView21 (WebView2) which is the Chromium Browser, not internet explorer, which is the default browser and is outdated.
    
    NOTE:
    If your Facebook Messenger can allow Drag and Drop of images that shows the images in chat, then do not waste your time with this.
    
    All you need is the .EXE

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    ''Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    ''End Sub
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Ensure that the WebView2 control is properly initialized
+        Await WebView21.EnsureCoreWebView2Async()
+        ' Navigate to Facebook.com/messages
+        WebView21.CoreWebView2.Navigate("https://www.facebook.com/messages")
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        WebView21.Height = Me.Height - 41
+        WebView21.Width = Me.Width - 18
+    End Sub
+End Class
