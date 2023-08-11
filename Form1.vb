1de9202964241aea6682cb77debc370d663105dc
LockMan007 <LockMan007@users.noreply.github.com>
2023-08-10 21:40:10 -0400

    Update Form1.vb
    revised commenting. no change in code.

--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,11 @@
 ﻿Public Class Form1
-
-    ''Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    ''End Sub
-
     Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Ensure that the WebView2 control is properly initialized
-        Await WebView21.EnsureCoreWebView2Async()
-        ' Navigate to Facebook.com/messages
-        WebView21.CoreWebView2.Navigate("https://www.facebook.com/messages")
+        Await WebView21.EnsureCoreWebView2Async() ' Ensure that the WebView2 control is properly initialized
+        WebView21.CoreWebView2.Navigate("https://www.facebook.com/messages") ' Navigate to Facebook.com/messages
     End Sub
 
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
-        WebView21.Height = Me.Height - 41
-        WebView21.Width = Me.Width - 18
+        WebView21.Height = Me.Height - 41 ' Resize WebView2 webbrowser height when you resize the window.
+        WebView21.Width = Me.Width - 18 ' same for width. Also subtracting some to give a 1 pixel border.
     End Sub
 End Class
