87d3edfb4c694179bdefc6d2bad7a6bb1aa33a33
LockMan007 <LockMan007@users.noreply.github.com>
2024-01-19 20:37:24 -0500

    v1.1
    v1.1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,14 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
         CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'WebView21
@@ -32,11 +35,26 @@ Partial Class Form1
         Me.WebView21.AllowExternalDrop = True
         Me.WebView21.CreationProperties = Nothing
         Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
-        Me.WebView21.Location = New System.Drawing.Point(1, 1)
+        Me.WebView21.Location = New System.Drawing.Point(12, 12)
         Me.WebView21.Name = "WebView21"
-        Me.WebView21.Size = New System.Drawing.Size(362, 226)
+        Me.WebView21.Size = New System.Drawing.Size(427, 263)
         Me.WebView21.TabIndex = 0
         Me.WebView21.ZoomFactor = 1.0R
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(70, 17)
+        Me.ToolStripStatusLabel1.Text = "Initializing..."
         '
         'Form1
         '
@@ -44,14 +62,20 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.WebView21)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Facebook Messenger - Toad Code 2023 (v1.0)"
+        Me.Text = "Facebook Messenger - Toad Code 2023 (v1.1)"
         CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,11 +1,41 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Web.WebView2.Core
+
+Public Class Form1
+
     Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebView21.Top = 1
+        WebView21.Left = 1
+
         Await WebView21.EnsureCoreWebView2Async() ' Ensure that the WebView2 control is properly initialized
-        WebView21.CoreWebView2.Navigate("https://www.facebook.com/messages") ' Navigate to Facebook.com/messages
+        ToolStripStatusLabel1.Text = "initialized"
+
+        WebView21.Source = New Uri("https://www.facebook.com/messages")
+
+        ' Attach relevant events
+        AddHandler WebView21.CoreWebView2.NavigationStarting, AddressOf WebViewNavigationStarting
+        AddHandler WebView21.CoreWebView2.NavigationCompleted, AddressOf WebViewNavigationCompleted
+    End Sub
+
+    Private Sub WebViewNavigationStarting(sender As Object, e As CoreWebView2NavigationStartingEventArgs)
+        'Update ToolStripStatusLabel for navigation starting
+        ToolStripStatusLabel1.Text = "Loading..."
+    End Sub
+
+    Private Sub WebViewNavigationCompleted(sender As Object, e As CoreWebView2NavigationCompletedEventArgs)
+        If e.IsSuccess Then
+            ' Update ToolStripStatusLabel for navigation completed to show the current URL
+            ToolStripStatusLabel1.Text = WebView21.Source.AbsoluteUri
+        Else
+            '      ' Update ToolStripStatusLabel for navigation error
+            ToolStripStatusLabel1.Text = $"Navigation Failed. Error Code: {e.WebErrorStatus}"
+            MsgBox($"Navigation Failed. Error Code: {e.WebErrorStatus}")
+            ' Alternatively: ToolStripStatusLabel1.Text = e.WebErrorStatus.ToString()
+        End If
     End Sub
 
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
-        WebView21.Height = Me.Height - 41 ' Resize WebView2 webbrowser height when you resize the window.
-        WebView21.Width = Me.Width - 18 ' same for width. Also subtracting some to give a 1 pixel border.
+        WebView21.Height = Me.Height - 64 '41
+        WebView21.Width = Me.Width - 18
     End Sub
+
 End Class
